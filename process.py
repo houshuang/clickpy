d697ef91a41f0bbc61a7eb971732d5b558bafe4f
Stian Håklev <shaklev@gmail.com>
2014-01-12 07:44:39 -0500

    cleaning fields, datetime

--- process.py
+++ process.py
@@ -8,11 +8,18 @@ import sys
 import ujson
 from urllib.parse import parse_qs
 import memo
+from collections import Counter
+from datetime import datetime
 
 C = 0
 
-unwanted_fields = ['data'] 
-memoizable_fields = ['action', 'page', 'quiz_id', 'type', 'visiting']
+unwanted_fields = ['data', 'user_ip', '12', '13', 'language', 'from',
+    'user_agent', 12, 13, 'page_url', 'client', 'networkState',
+    'visiting', 'error', 'lecture_player', 'minimal', 'readyState',
+    'user_id', 'value'] 
+memoizable_fields = ['action', 'page', 'quiz_id', 'type', 'visiting',
+    'key', 'session', 'username']
+timestamp_fields = ['initTimestamp', 'eventTimestamp', 'timestamp']
 
 memos = {key:memo.Memo() for key in memoizable_fields}
 
@@ -37,7 +44,6 @@ def get_prefix_size(fname):
         prefix = r"https://class.coursera.org/(.+?)/"
         return(len(re.match(prefix, firstparse['page_url']).group(0)))
 
-
 def memoize(action):
     # remove fields we don't care about
     for field in unwanted_fields:
@@ -47,8 +53,13 @@ def memoize(action):
     for field, memoizer in memos.items():
         if field in action: 
             action[field] = memoizer[action[field]]
-
     return action
+
+def nan_or_timestamp(val):
+    if not (pd.isnull(val)):
+        val = datetime.fromtimestamp(int(val) / 1000.)
+    return val
+
 
 def main(fname):
     
@@ -58,23 +69,28 @@ def main(fname):
     with open(fname) as f:
         for line in f:
             parsestr = ujson.loads(line)
-            if parsestr['key'] != "pageview":
-                continue
+
+            if parsestr['key'] == "user.video.lecture.action":
+                parsestr.update(ujson.loads(parsestr["value"]))
+
             urlparse = parse(parsestr['page_url'], prefix_size)
             parsestr.update(urlparse)
+
             arr.append(memoize(parsestr))
 
-    print(DataFrame(arr).columns())
-    
-    for field, memoizer in memos.items():
-        print(field)
-        print(memoizer.toHash())
+    db = DataFrame(arr)
 
+    for col in timestamp_fields:
+        db[col] = db[col].apply(nan_or_timestamp)
+    print(db)
+    for col in db.columns:
+        print("%s: %i, %i" % (col, db[col].count(), len(db[col].unique())))
+    return(db)
 
 
 if __name__ == "__main__":
 
     print("Starting...")
-    fname = "/Users/Stian/Dropbox/Laurie/Enc/clicklog-prog2-1k"
+    fname = "/Users/Stian/src/clickpy/introstats_001_10k"
 
-    main(fname)
+    db=main(fname)
