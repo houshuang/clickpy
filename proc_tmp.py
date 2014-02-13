8e7e52424aa31ce665c706b539670d662014354d
Stian Håklev <shaklev@gmail.com>
2014-02-13 13:56:28 -0500

    beginning of async write for step 2 processing

---
+++ proc_tmp.py
@@ -0,0 +1,38 @@
+from time import perf_counter
+import os
+import time
+import pickle
+import sys
+from pandas import DataFrame
+import pandas as pd
+import numpy as np
+
+tmpdir = sys.argv[1]
+storepath = sys.argv[2]
+if os.path.exists(storepath):
+    os.remove(storepath)
+store = pd.HDFStore(storepath, "w")
+
+while True:
+    t = perf_counter()
+
+    files = os.listdir(tmpdir)
+    a = [f for f in files if os.path.isfile(os.path.join(tmpdir,f))
+        and f != "finished.signal"]
+    if a == []:
+        if "finished.signal" in files:
+            break
+        time.sleep(1)
+        continue
+    fname = os.path.join(tmpdir, a[0])
+    arr = pickle.load(open(fname, "rb"))
+    print(">>> Opening %s, %d units" % (a[0], len(arr)))
+
+    store.append('db-video-proc', arr)
+    os.remove(fname)
+    print(">>> %s: %f" % (a[0], perf_counter()-t))
+
+os.remove(os.path.join(tmpdir, "finished.signal"))
+
+store.close()
+print("### Finished")
