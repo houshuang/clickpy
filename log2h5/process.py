93e228d6b009a1ec4a54b18c636200b884c3363d
Stian Håklev <shaklev@gmail.com>
2014-02-23 21:57:53 -0500

    Process with redis seems functional - tested in single-mode

--- log2h5/process.py
+++ log2h5/process.py
@@ -1,3 +1,5 @@
+import pickle
+from uuid import uuid4
 import os
 import re
 import sys
@@ -13,241 +15,224 @@ from urllib.parse import parse_qs
 from pandas import DataFrame, Series
 import pandas as pd
 import numpy as np
-
+import redis
 import memo
 
-# fields that will be automatically memoized
-memoizable_fields = ['action', 'page', 'type', 'visiting',
-                     'key', 'session', 'username']
-memos = {key:memo.Memo(key) for key in memoizable_fields}
+class Logger(object):
+	def __init__(self, r, prefix):
+		self.r = r
+		self.log = '%s:log' % prefix
 
-# we'll strip off the nanosecond counter
-# timestamp conversion is easier with Pandas afterwards
-timestamp_fields = ['initTimestamp', 'eventTimestamp', 'timestamp']
+	def log(self, text):
+		self.r.rpush(self.log, text)
 
-# these are the columns that we are keeping, all others are
-# removed
-cols = ['action', 'currentTime', 'eventTimestamp', 'forum_id',
-        'initTimestamp', 'key', 'lecture_id', 'page', 'paused',
-        'playbackRate', 'post_id', 'prevTime', 'quiz_id', 'session',
-        'submission_id', 'thread_id', 'timestamp', 'type', 'username']
+class Process(object):
+	def get_prefix_size(self, fname):
+		"""Reads one line of the file to determine the prefix"""
+		with open(fname) as f:
+			firstline = f.readline()
+			firstparse = ujson.loads(firstline)
+			prefix = r"https://class.coursera.org/(.+?)/"
+			return(len(re.match(prefix, firstparse['page_url']).group(0)))
 
+	def __init__(self, argv):
+		if len(argv) < 2:
+			print("Usage: python process.py tmpdir prefix")
+			exit()
 
-def clean_number(nstr):
-    try:
-        a = int(not_num_re.sub('', nstr))
-    except:
-        a = np.NaN
-    return(a)
+		self.tmpdir = argv[1]
+		self.prefix = argv[2]
+		self.prefix_size = None
 
-def unwrap_hash(h):
-    return( {k:v[0] for k,v in h.items()} )
+		# fields that will be automatically memoized
+		self.memoizable_fields = ['action', 'page', 'type', 'visiting',
+							 'key', 'session', 'username']
+		self.memos = {key:memo.Memo(key, self.prefix) for key in self.memoizable_fields}
 
-def clean_value(v):
-    if '#' in v:
-        v = v.split('#')[0]
-    return(v)
+		# we'll strip off the nanosecond counter
+		# timestamp conversion is easier with Pandas afterwards
+		self.timestamp_fields = ['initTimestamp', 'eventTimestamp', 'timestamp']
 
+		# these are the columns that we are keeping, all others are
+		# removed
+		self.cols = ['action', 'currentTime', 'eventTimestamp', 'forum_id',
+				'initTimestamp', 'key', 'lecture_id', 'page', 'paused',
+				'playbackRate', 'post_id', 'prevTime', 'quiz_id', 'session',
+				'submission_id', 'thread_id', 'timestamp', 'type', 'username']
 
-# precompiling regexps for speed
-num_re = re.compile("\d")
-not_num_re = re.compile('[^\d]')
-human_grading_re = re.compile("human_grading/view/courses/\d+/assessments/\d+/?(.+?)?/*\d*$")
-lecture_re = re.compile("lecture/(\d+)")
+		# precompiling regexps for speed
+		self.num_re = re.compile("\d")
+		self.not_num_re = re.compile('[^\d]')
+		self.human_grading_re = re.compile("human_grading/view/courses/\d+/assessments/\d+/?(.+?)?/*\d*$")
+		self.lecture_re = re.compile("lecture/(\d+)")
 
-def parse(url, prefix_size):
-    url, *part = url.strip().split('?', 1)
+		self.pid = os.getpid()
+		self.r = redis.StrictRedis(host='localhost', decode_responses=True)
+		logger = Logger(self.r, self.prefix)
+		self.npobj = np.dtype('O')
 
-    # we remove the course prefix and everything after # from URL string
-    # even if there is no "#", getting the first element will always
-    # work
-    actionstr = url[prefix_size:].split("#")[0]
-    action = {}
-    # first check if any numbers, to skip longer matches
-    # try:
-    if re.search(num_re, actionstr):
-        # special parsing for human grading actions
-        match = re.match(human_grading_re, actionstr)
-        if not match is None:
-            actionstr = "human_grading/"
-            return({'action': actionstr})
+	def clean_number(self, nstr):
+		try:
+			a = int(self.not_num_re.sub('', nstr))
+		except:
+			a = np.NaN
+		return(a)
 
-        # special parsing for lecture_views, otherwise proceed as normal
-        match = re.search(lecture_re, actionstr)
-        if not match is None:
-            return({'action': 'lecture_view',
-                    'lecture_id': match.groups()[0]})
+	def unwrap_hash(self, h):
+		return( {k:v[0] for k,v in h.items()} )
 
+	def clean_value(self, v):
+		if '#' in v:
+			v = v.split('#')[0]
+		return(v)
 
-    action = {'action': actionstr}
+	def parse(self, url):
+		url, *part = url.strip().split('?', 1)
 
-    if "human_grading" in action['action']:
-        return({'action': 'human_grading/'})
+		# we remove the course prefix and everything after # from URL string
+		# even if there is no "#", getting the first element will always
+		# work
+		actionstr = url[self.prefix_size:].split("#")[0]
+		action = {}
+		# first check if any numbers, to skip longer matches
+		# try:
+		if re.search(self.num_re, actionstr):
+			# special parsing for human grading actions
+			match = re.match(self.human_grading_re, actionstr)
+			if not match is None:
+				actionstr = "human_grading/"
+				return({'action': actionstr})
 
-
-    # process any url arguments (?arg=opt)
-    if part:
-        items = unwrap_hash(parse_qs(part[0]))
-        items = {k:clean_value(v) for k,v in items.items()}
-        action.update(items)
-
-    return(action)
-
-
-def get_prefix_size(fname):
-    """Reads one line of the file to determine the prefix"""
-    with open(fname) as f:
-        firstline = f.readline()
-        firstparse = ujson.loads(firstline)
-        prefix = r"https://class.coursera.org/(.+?)/"
-        return(len(re.match(prefix, firstparse['page_url']).group(0)))
-
-
-def memoize(df):
-    """Runs through list of fields to memoize, and returns
-    new dataframe with all fields memoized"""
-    for field, memoizer in memos.items():
-        if field in df.columns:
-            df[field] = df[field].apply(memoizer.get)
-    return df
+			# special parsing for lecture_views, otherwise proceed as normal
+			match = re.search(self.lecture_re, actionstr)
+			if not match is None:
+				return({'action': 'lecture_view',
+						'lecture_id': match.groups()[0]})
 
 
-# flatten only once - ugly, what's a better way?
-def flatten(items, onlyonce=True):
-    for x in items:
-        if onlyonce:
-            yield from flatten(x, False)
-        else:
-            yield x
+		action = {'action': actionstr}
+
+		if "human_grading" in action['action']:
+			return({'action': 'human_grading/'})
 
 
-Npobj = np.dtype('O')
+		# process any url arguments (?arg=opt)
+		if part:
+			items = self.unwrap_hash(parse_qs(part[0]))
+			items = {k:self.clean_value(v) for k,v in items.items()}
+			action.update(items)
 
-def storeappend(store, arr):
-    if not arr or arr == []:
-        return
+		return(action)
 
-    a = DataFrame(arr, columns=cols)
-    a = memoize(a)
-
-    # there should not be any strings left, but we need to clean the
-    # numbers and convert to floats
-
-    for col in a.columns:
-        if a[col].dtype == Npobj:
-            a[col] = a[col].apply(clean_number)
-        a[col] = a[col].astype(np.float64)
-
-    store.append('db', a, index=False)
-    del a
+	def memoize(self, df):
+		"""Runs through list of fields to memoize, and returns
+		new dataframe with all fields memoized"""
+		for field, memoizer in self.memos.items():
+			if field in df.columns:
+				df[field] = df[field].apply(memoizer.get)
+				print("memoizing %s" % field)
+		return df
 
 
-def fix_timestamp(parsestr):
-    """Remove last three digits (nanoseconds) from all timestamp fields"""
-    for field in timestamp_fields:
-        if field in parsestr:
-            if isinstance(parsestr[field], int):
-                parsestr[field] = parsestr[field]/1000
-            elif isinstance(parsestr[field], str):
-                parsestr[field] = parsestr[field][:-3]
-    return(parsestr)
+	# flatten only once - ugly, what's a better way?
+	def flatten(self, items, onlyonce=True):
+		for x in items:
+			if onlyonce:
+				yield from flatten(x, False)
+			else:
+				yield x
+
+	def dump(self, arr):
+		"""Dumps lines to a text file in tmp dir"""
+
+		idstr = str(uuid4())
+		tmpname = os.path.join(self.tmpdir, "sub", idstr)
+		dumpname = os.path.join(self.tmpdir, "dump", idstr)
+		with open(tmpname, "wb") as dumpf:
+			pickle.dump(arr, dumpf)
+		os.rename(tmpname, dumpname)
+
+		print("*** %d: Dumping to %s" % (self.pid, idstr))
+
+	def storeappend(self, arr):
+		if not arr or arr == []:
+			return
+
+		a = DataFrame(arr, columns=self.cols)
+		a = self.memoize(a)
+
+		# there should not be any strings left, but we need to clean the
+		# numbers and convert to floats
+
+		for col in a.columns:
+			if a[col].dtype == self.npobj:
+				a[col] = a[col].apply(self.clean_number)
+			a[col] = a[col].astype(np.float64)
+
+		self.dump(a)
+
+	def fix_timestamp(self, parsestr):
+		"""Remove last three digits (nanoseconds) from all timestamp fields"""
+		for field in self.timestamp_fields:
+			if field in parsestr:
+				if isinstance(parsestr[field], int):
+					parsestr[field] = parsestr[field]/1000
+				elif isinstance(parsestr[field], str):
+					parsestr[field] = parsestr[field][:-3]
+		return(parsestr)
+
+	def process(self, line):
+		parsestr = ujson.loads(line)
+
+		if parsestr['key'] == "user.video.lecture.action":
+			parsestr.update(ujson.loads(parsestr["value"]))
+
+		urlparse = self.parse(parsestr['page_url'])
+		parsestr.update(urlparse)
+		parsestr = self.fix_timestamp(parsestr)
+		return(parsestr)
 
 
-def process(prefix_size, line):
-    parsestr = ujson.loads(line)
+	def start(self):
+		# do as long as there are still files to process
 
-    if parsestr['key'] == "user.video.lecture.action":
-        parsestr.update(ujson.loads(parsestr["value"]))
+		while True:
+			files = os.listdir(self.tmpdir)
+			a = [f for f in files if os.path.isfile(os.path.join(self.tmpdir, f))]
+			if a == []:
+				break # done processing
 
-    urlparse = parse(parsestr['page_url'], prefix_size)
-    parsestr.update(urlparse)
-    parsestr = fix_timestamp(parsestr)
-    return(parsestr)
+			nextfile = a[0]
+			fname = os.path.join(self.tmpdir, nextfile) # grab the first file we see
+			fname_sub = os.path.join(self.tmpdir, "sub", nextfile)
 
-# 4, 10,000 : 17
-# 8, 10,000 : 17
-# no effect of parallelization? too small chunks? try bigger chunks,
-# or on server...
+			try:
+				os.rename(fname, fname_sub) # move it, to avoid others getting their hands on it
+			except:
+				continue # someone else grabbed the file, try again
 
-def proc_chunk(linearr, p, store):
-    arr = p.map(procfunc, linearr)
-    storeappend(store, arr)
+			print(">>> %d: Opening %s" % (self.pid, nextfile))
 
-def main(config):
-    hdf = config.clicklog + (".h5")
-    if os.path.exists(hdf):
-        os.remove(hdf)
+			if not self.prefix_size:
+				self.prefix_size = self.get_prefix_size(fname_sub)
 
-    store = pd.HDFStore(hdf, "w")
+			proclines = []
+			with open(fname_sub) as f:
+				for line in f:
+					#try:
+						proclines.append(self.process(line))
+					#except:
+					#	self.logger.log("Failed parse: %s" % line)
 
-    p = Pool(config.pool_size)
-    chunksize = config.chunk_size
+			self.storeappend(proclines)
+			os.remove(fname_sub)
+			print(">>> %d: Done with %s" % (self.pid, nextfile))
 
-    i = 0
-    arr = []
-    linearr = []
-
-    with open(config.clicklog) as f:
-        for line in f:
-            i += 1
-            linearr.append(line)
-
-            if i / chunksize == i // chunksize:
-                proc_chunk(linearr, p, store)
-                linearr = []
-
-                print(i)
-
-        # if more lines left, process now
-        if len(linearr) > 0:
-            proc_chunk(linearr, p, store)
-        del linearr
-
-
-    store.close()
-    hdf_repack = config.clicklog + '.h5.repacked'
-    if os.path.exists(hdf_repack):
-        os.remove(hdf_repack)
-
-
-    print("Finished, now repacking with index")
-    store = pd.HDFStore(config.clicklog + ".h5")
-    db = store['db']
-    db.to_hdf(hdf_repack,'db',mode='w',format='table',index=['timestamp'],
-              data_columns=['action', 'username', 'timestamp'])
-    store.close()
-
-    store = pd.HDFStore(hdf_repack)
-    print("Storing memoized data")
-    for field, memoizer in memos.items():
-        memoizer.store(store)
-    store.close()
-
-    print("\nStatistics, unique values: (fields with M are memoized)")
-    for col in db.columns:
-        memoized = "M" if col in memoizable_fields else ""
-        print(col, memoized, ": ", len(db[col].unique()))
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(prog = 'clickpy',
-        description='Process clicklog files')
-    parser.add_argument('--pool-size', '-p', type=int, metavar='n', default=6,
-        help="Size of pool for concurrent processing of lines (default 6)")
-    parser.add_argument('--chunk-size', '-c', type=int, metavar='n', default=200000,
-        help="Chunk of lines distributed amongst pools for each run (default 200,000)")
-    parser.add_argument('--working-dir', '-d', type=str, metavar='x', default='/tmp/clickpy',
-        help="Working workingdirectory, will be deleted on start (default /tmp/clickpy)")
-    parser.add_argument('clicklog',type=str,
-        help="clicklog file")
-
-    return(parser.parse_args())
-
+		# no more files, send sentinel and wrap up
+		self.r.rpush('%s:finished' % (self.prefix) , self.pid)
+		print(">>> %d: Finished with all files, shutting down" % self.pid)
 
 if __name__ == "__main__":
-    config = parse_args()
-    print("Starting...")
-    prefix_size = get_prefix_size(config.clicklog)
-    procfunc = partial(process, prefix_size)
-
-    main(config)
+	print("Starting...")
+	proc = Process(sys.argv)
+	proc.start()
